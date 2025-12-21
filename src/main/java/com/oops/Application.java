02bece84cef5eb687bef25fcd813f609d8b4ca60
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2025-12-22 03:40:34 +0900

    chore: OAuth 관련 설정 및 테스트 환경 추가

--- src/main/java/com/oops/Application.java
+++ src/main/java/com/oops/Application.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.Environment;
 
@@ -13,6 +14,7 @@ import java.util.TimeZone;
 
 @Slf4j
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @RequiredArgsConstructor
 public class Application implements ApplicationListener<ApplicationReadyEvent> {
     private final Environment environment;
